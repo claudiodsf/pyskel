835bf25fd49341ba11cba69f0b69cfa2a90eb9ff
Claudio Satriano <satriano@gmail.com>
2024-09-26 08:35:39 +0200

    Remove python2 compatibility code

--- set_package_and_author.py
+++ set_package_and_author.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This script sets the package name and author name and email in the source
+"""
+This script sets the package name and author name and email in the source
 code of a Python package created with pyskel.
 
 The script will ask for the package name, author name and author email,
@@ -11,42 +12,40 @@ inputs.
 Files that match the ignore patterns in ".gitignore" will be skipped.
 """
 import os
+import contextlib
 import textwrap
 import subprocess
-try:
-    raw_input
-except NameError:
-    raw_input = input
 
 
 def main():
+    """
+    Main function of the script.
+    """
     print(textwrap.fill(__doc__, 80))
     print()
-    answer = raw_input("Continue? [y/n] ")
+    answer = input("Continue? [y/n] ")
     if answer.lower() != "y":
         return
     # Interactive prompt for package name, author name and author email
     # the answer cannot be empty
     while True:
-        package_name = raw_input("Package name: ")
+        package_name = input("Package name: ")
         if package_name:
             break
     while True:
-        author_name = raw_input("Author name: ")
+        author_name = input("Author name: ")
         if author_name:
             break
     while True:
-        author_email = raw_input("Author email: ")
+        author_email = input("Author email: ")
         if author_email:
             break
     # Build a file list using the system command "git -ls-files"
     filelist = subprocess.check_output(
         'git ls-files', shell=True).splitlines()
     # Remove the file "set_package_and_author.py" from the list
-    try:
+    with contextlib.suppress(ValueError):
         filelist.remove(b"set_package_and_author.py")
-    except ValueError:
-        pass
     # Replace the strings "pyskel", "Author Name" and "author@email.com"
     # with the provided inputs for every file in the list
     for filename in filelist:
